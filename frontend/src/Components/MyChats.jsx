7b9cd3ec83036bc1dd336b9858de1807578b0368
SK-WebDeveloper <vermasanjaykumar97@gmail.com>
2024-06-28 08:03:25 +0530

    eslint disable

--- frontend/src/Components/MyChats.jsx
+++ frontend/src/Components/MyChats.jsx
@@ -40,6 +40,7 @@ const MyChats = ({fetchAgain}) => {
   useEffect(() => {
     setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
+    // eslint-disable-next-line
   }, [fetchAgain]);
   return (
     <Box
